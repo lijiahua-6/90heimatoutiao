c6705ce41dbc7f599bdcfde4948850f1d9f52e6f
lijiahua <lijiahua@163.com>
2019-12-24 19:11:10 +0800

    素材列表-素材的收藏和删除

--- src/utils/request.js
+++ src/utils/request.js
@@ -18,7 +18,7 @@ axios.interceptors.request.use(function (config) {
 })
 // 后台数据 到达 响应拦截之前走的一个函数
 axios.defaults.transformResponse = [function (data) {
-  return JSONBig.parse(data) // JSONbig.parse 替换 JSON.parse  保证数字的正确
+  return data.length ? JSONBig.parse(data) : {} // JSONbig.parse 替换 JSON.parse  保证数字的正确
 }]
 // 响应拦截
 axios.interceptors.response.use(function (response) {
