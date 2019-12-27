34fa221c1f088fce024bfeda01a7464f57519dc8
lijiahua <lijiahua@163.com>
2019-12-27 17:52:47 +0800

    修改文章，简单模式跳转

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -76,7 +76,7 @@
        <!-- 右侧 -->
        <el-col :span="6">
            <el-row class='right' type='flex' justify="end">
-               <span><i class="el-icon-edit"></i>修改</span>
+               <span @click="toModify(item.id)"><i class="el-icon-edit"></i>修改</span>
                <span @click="delArticle(item.id)"><i  class="el-icon-delete"></i> 删除</span>
            </el-row>
        </el-col>
@@ -151,6 +151,10 @@ export default {
     }
   },
   methods: {
+    // 去修改页面
+    toModify (id) {
+      this.$router.push(`/home/publish/${id.toString()}`) // 到发布页面
+    },
     // 删除文章
     delArticle (id) {
       // 所有已发布的文章是不可以删除的  只有草稿才可以删除
